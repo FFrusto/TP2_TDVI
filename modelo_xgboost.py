b201b8f59c6e5a1ccb6a1bfb72470b2a74a5347e
Valentin-Prieto <126887481+Valentin-Prieto@users.noreply.github.com>
2023-09-03 17:47:59 -0300

    modelo con XGBoost

--- modelo_xgboost.py
+++ modelo_xgboost.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import gc
 from sklearn.impute import SimpleImputer
-from modelo_xgboost import XGBClassifier
+from xgboost import XGBClassifier  # Importar el modelo XGBoost
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
 
 # Load the competition data
 comp_data = pd.read_csv("competition_data.csv")
@@ -32,24 +31,8 @@ del train_data
 gc.collect()
 
 # Cambiar el modelo a XGBoost
-cls = make_pipeline(SimpleImputer(), XGBClassifier(max_depth=10, random_state=2345))  
-
-# Realiza validación cruzada con k-fold (por ejemplo, k=5)
-cv_scores = cross_val_score(cls, X_train, y_train, cv=6, scoring='roc_auc')
-
-# Calcula el promedio de los puntajes de validación cruzada
-mean_cv_score = cv_scores.mean()
-print("Promedio de ROC AUC en validación cruzada:", mean_cv_score)
-
-# Entrena el modelo final en todos los datos de entrenamiento
+cls = make_pipeline(SimpleImputer(), XGBClassifier(max_depth=10, random_state=2345))  # Usar XGBClassifier en lugar de DecisionTreeClassifier
 cls.fit(X_train, y_train)
-
-# Calcula las predicciones en el conjunto de prueba
-y_pred_test = cls.predict_proba(X_test)[:, cls.classes_ == 1].squeeze()
-
-# Calcula el ROC AUC en el conjunto de prueba
-roc_auc_test = roc_auc_score(y_test, y_pred_test)
-print("ROC AUC en conjunto de prueba:", roc_auc_test)
 
 # Predict on the evaluation set
 eval_data = eval_data.drop(columns=["conversion"])
@@ -60,3 +43,5 @@ y_preds = cls.predict_proba(eval_data.drop(columns=["ROW_ID"]))[:, cls.classes_ 
 submission_df = pd.DataFrame({"ROW_ID": eval_data["ROW_ID"], "conversion": y_preds})
 submission_df["ROW_ID"] = submission_df["ROW_ID"].astype(int)
 submission_df.to_csv("xgboost_model.csv", sep=",", index=False)
+
+print(cls.score(X_train, y_train))
